00d9eab3af908fbdbf4e1032bf295609a4d1bfd3
Azeez Temitope Olanrewaju <59709429+lanru2001@users.noreply.github.com>
2021-10-22 10:39:16 -0500

    Update lambda_function.tf

--- aws/lambda_function.tf
+++ aws/lambda_function.tf
@@ -1,3 +1,36 @@
+resource "aws_cloudwatch_log_group" "example" {
+  name              = "/aws/lambda/${var.lambda_function_name}"
+  retention_in_days = 14
+}
+
+# See also the following AWS managed policy: AWSLambdaBasicExecutionRole
+resource "aws_iam_policy" "lambda_logging" {
+  name        = "lambda_logging"
+  path        = "/"
+  description = "IAM policy for logging from a lambda"
+
+  policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Action": [
+        "logs:CreateLogGroup",
+        "logs:CreateLogStream",
+        "logs:PutLogEvents"
+      ],
+      "Resource": "arn:aws:logs:*:*:*",
+      "Effect": "Allow"
+    }
+  ]
+}
+EOF
+}
+
+resource "aws_iam_role_policy_attachment" "lambda_logs" {
+  role       = aws_iam_role.iam_for_lambda.name
+  policy_arn = aws_iam_policy.lambda_logging.arn
+}
 
 resource "aws_lambda_function" "test_lambda" {
   filename      = "lambda_function_payload.zip"
@@ -11,7 +44,11 @@ resource "aws_lambda_function" "test_lambda" {
   source_code_hash = filebase64sha256("lambda_function_payload.zip")
 
   runtime = "nodejs12.x"
-
+  
+  depends_on = [
+    aws_iam_role_policy_attachment.lambda_logs,
+    aws_cloudwatch_log_group.example,
+  ]
   environment {
     variables = {
       foo = "bar"
