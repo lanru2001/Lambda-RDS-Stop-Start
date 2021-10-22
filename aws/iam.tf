c19153a45092795bfa5818a1bbc83b589f4207bf
Azeez Temitope Olanrewaju <59709429+lanru2001@users.noreply.github.com>
2021-10-22 11:04:15 -0500

    Update iam.tf

--- aws/iam.tf
+++ aws/iam.tf
@@ -3,18 +3,28 @@ resource "aws_iam_role" "iam_for_lambda" {
 
   assume_role_policy = <<EOF
 {
-  "Version": "2012-10-17",
-  "Statement": [
-    {
-      "Action": "sts:AssumeRole",
-      "Principal": {
-        "Service": "lambda.amazonaws.com"
-      },
-      "Effect": "Allow",
-      "Sid": ""
-    }
-  ]
-}
+    "Version": "2012-10-17",
+    "Statement": [
+        {           
+            "Effect": "Allow",
+            "Action": [
+                "rds:DescribeDBInstances",
+                "rds:StopDBInstance",
+                "rds:StartDBInstance"
+            ],
+            "Resource": "*"
+        },
+        {
+            "Effect": "Allow",
+            "Action": [
+                "logs:CreateLogGroup",
+                "logs:CreateLogStream",
+                "logs:PutLogEvents"
+            ],
+            "Resource": "*"
+        }
+    ]
+}    
 EOF
 }
 
