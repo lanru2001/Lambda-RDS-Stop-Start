52e58ab980afe32047bff26f7f35a19951f54ae0
Azeez Temitope Olanrewaju <59709429+lanru2001@users.noreply.github.com>
2021-10-23 14:08:16 -0500

    Update lambda_function.tf

--- aws/lambda_function.tf
+++ aws/lambda_function.tf
@@ -5,10 +5,6 @@ data "archive_file" "lambda"{
 
 }
 
-resource "aws_cloudwatch_log_group" "example" {
-  name              = "/aws/lambda/${var.lambda_function_name}"
-  retention_in_days = 14
-}
 
 # See also the following AWS managed policy: AWSLambdaBasicExecutionRole
 resource "aws_iam_policy" "lambda_logging" {
