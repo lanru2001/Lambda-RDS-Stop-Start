1b4e0f82024db7c0354da1e549b12d2262fae4e5
Azeez Temitope Olanrewaju <59709429+lanru2001@users.noreply.github.com>
2021-10-23 14:31:53 -0500

    Update cloudwatch.tf

--- aws/cloudwatch.tf
+++ aws/cloudwatch.tf
@@ -5,19 +5,16 @@ resource "aws_cloudwatch_log_group" "lambda_log" {
 }
 
 # Cloudwatch event rule
-resource "aws_cloudwatch_event_rule" "check-scheduler-event" {
-  name                = "${var.resource_name_prefix}check-scheduler-event"
+resource "aws_cloudwatch_event_rule" "check_scheduler_event" {
+  name                = var.check_cheduler_rule
   description         = "check-scheduler-event"
   schedule_expression = var.schedule_expression
-  depends_on          = [aws_lambda_function.scheduler_lambda]
+  depends_on          = [aws_lambda_function.lambda_test]
 }
-
 
 # Cloudwatch event target
 resource "aws_cloudwatch_event_target" "check-scheduler-event-lambda-target" {
   target_id = "check-scheduler-event-lambda-target"
-  rule      = aws_cloudwatch_event_rule.check-scheduler-event.name
-  arn       = aws_lambda_function.scheduler_lambda.arn
+  rule      = aws_cloudwatch_event_rule.check_scheduler_event.name
+  arn       = aws_lambda_function.lambda_test.arn
 }
-
-
