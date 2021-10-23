108a3a85eb4697426d9fed433c9bf81a053a8afd
Azeez Temitope Olanrewaju <59709429+lanru2001@users.noreply.github.com>
2021-10-23 15:03:08 -0500

    Update lambda_function.tf

--- aws/lambda_function.tf
+++ aws/lambda_function.tf
@@ -14,8 +14,6 @@ resource "aws_lambda_function" "test_lambda" {
   handler          = "index.test"
   timeout          = 300
   # The filebase64sha256() function is available in Terraform 0.11.12 and later
-  # For Terraform 0.11.11 and earlier, use the base64sha256() function and the file() function:
-  # source_code_hash = "${base64sha256(file("lambda_function_payload.zip"))}"
   #source_code_hash = filebase64sha256("lambda_function_payload.zip")
   source_code_hash = data.archive_file.lambda_file.output_base64sha256
   vpc_config {
